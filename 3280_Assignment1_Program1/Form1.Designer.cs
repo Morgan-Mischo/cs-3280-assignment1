1f6583c7fb008f1648ad5c3387262bd3c70092d9
Morgan Mischo <goramorgan@gmail.com>
2022-12-24 13:00:12 -0700

    Upload from Fall 22 Semester
    Assignment 1 from CS 3280 - Object Oriented Windows using C# at Weber State University with Shawn Crowder

---
+++ 3280_Assignment1_Program1/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace _3280_Assignment1_Program1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.phone_number_text = new System.Windows.Forms.TextBox();
+            this.credit_card_text = new System.Windows.Forms.TextBox();
+            this.phone_number_no = new System.Windows.Forms.Button();
+            this.phone_number_prompt = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.phone_number_q = new System.Windows.Forms.Label();
+            this.credit_card_q = new System.Windows.Forms.Label();
+            this.credit_card_submit = new System.Windows.Forms.Button();
+            this.credit_card_prompt = new System.Windows.Forms.Label();
+            this.phone_number_submit = new System.Windows.Forms.Button();
+            this.credit_card_no = new System.Windows.Forms.Button();
+            this.bank_info_q = new System.Windows.Forms.Label();
+            this.bank_info_text = new System.Windows.Forms.TextBox();
+            this.bank_info_prompt = new System.Windows.Forms.Label();
+            this.bank_info_submit = new System.Windows.Forms.Button();
+            this.bank_info_no = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.BackColor = System.Drawing.SystemColors.Highlight;
+            this.title.Location = new System.Drawing.Point(60, 21);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(296, 23);
+            this.title.TabIndex = 5;
+            // 
+            // phone_number_text
+            // 
+            this.phone_number_text.Location = new System.Drawing.Point(114, 96);
+            this.phone_number_text.Name = "phone_number_text";
+            this.phone_number_text.Size = new System.Drawing.Size(173, 20);
+            this.phone_number_text.TabIndex = 6;
+            // 
+            // credit_card_text
+            // 
+            this.credit_card_text.Location = new System.Drawing.Point(114, 331);
+            this.credit_card_text.Name = "credit_card_text";
+            this.credit_card_text.Size = new System.Drawing.Size(173, 20);
+            this.credit_card_text.TabIndex = 7;
+            // 
+            // phone_number_no
+            // 
+            this.phone_number_no.Location = new System.Drawing.Point(212, 197);
+            this.phone_number_no.Name = "phone_number_no";
+            this.phone_number_no.Size = new System.Drawing.Size(75, 23);
+            this.phone_number_no.TabIndex = 13;
+            this.phone_number_no.Text = "Nope";
+            this.phone_number_no.Click += new System.EventHandler(this.phone_number_no_Click);
+            // 
+            // phone_number_prompt
+            // 
+            this.phone_number_prompt.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.phone_number_prompt.Location = new System.Drawing.Point(60, 141);
+            this.phone_number_prompt.Name = "phone_number_prompt";
+            this.phone_number_prompt.Size = new System.Drawing.Size(296, 23);
+            this.phone_number_prompt.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(60, 554);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(296, 23);
+            this.label5.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(60, 595);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(296, 23);
+            this.label6.TabIndex = 20;
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(60, 635);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(296, 23);
+            this.label7.TabIndex = 21;
+            // 
+            // phone_number_q
+            // 
+            this.phone_number_q.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.phone_number_q.Location = new System.Drawing.Point(60, 55);
+            this.phone_number_q.Name = "phone_number_q";
+            this.phone_number_q.Size = new System.Drawing.Size(296, 23);
+            this.phone_number_q.TabIndex = 22;
+            // 
+            // credit_card_q
+            // 
+            this.credit_card_q.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.credit_card_q.Location = new System.Drawing.Point(60, 266);
+            this.credit_card_q.Name = "credit_card_q";
+            this.credit_card_q.Size = new System.Drawing.Size(296, 23);
+            this.credit_card_q.TabIndex = 23;
+            // 
+            // credit_card_submit
+            // 
+            this.credit_card_submit.Location = new System.Drawing.Point(114, 448);
+            this.credit_card_submit.Name = "credit_card_submit";
+            this.credit_card_submit.Size = new System.Drawing.Size(75, 23);
+            this.credit_card_submit.TabIndex = 24;
+            this.credit_card_submit.Text = "Submit";
+            this.credit_card_submit.Click += new System.EventHandler(this.credit_card_submit_Click);
+            // 
+            // credit_card_prompt
+            // 
+            this.credit_card_prompt.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.credit_card_prompt.Location = new System.Drawing.Point(60, 397);
+            this.credit_card_prompt.Name = "credit_card_prompt";
+            this.credit_card_prompt.Size = new System.Drawing.Size(296, 23);
+            this.credit_card_prompt.TabIndex = 25;
+            // 
+            // phone_number_submit
+            // 
+            this.phone_number_submit.Location = new System.Drawing.Point(114, 197);
+            this.phone_number_submit.Name = "phone_number_submit";
+            this.phone_number_submit.Size = new System.Drawing.Size(75, 23);
+            this.phone_number_submit.TabIndex = 26;
+            this.phone_number_submit.Text = "Submit";
+            this.phone_number_submit.Click += new System.EventHandler(this.phone_number_submit_Click);
+            // 
+            // credit_card_no
+            // 
+            this.credit_card_no.Location = new System.Drawing.Point(212, 448);
+            this.credit_card_no.Name = "credit_card_no";
+            this.credit_card_no.Size = new System.Drawing.Size(75, 23);
+            this.credit_card_no.TabIndex = 27;
+            this.credit_card_no.Text = "Nope";
+            this.credit_card_no.Click += new System.EventHandler(this.credit_card_no_Click);
+            // 
+            // bank_info_q
+            // 
+            this.bank_info_q.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.bank_info_q.Location = new System.Drawing.Point(60, 503);
+            this.bank_info_q.Name = "bank_info_q";
+            this.bank_info_q.Size = new System.Drawing.Size(296, 23);
+            this.bank_info_q.TabIndex = 28;
+            // 
+            // bank_info_text
+            // 
+            this.bank_info_text.Location = new System.Drawing.Point(114, 557);
+            this.bank_info_text.Name = "bank_info_text";
+            this.bank_info_text.Size = new System.Drawing.Size(173, 20);
+            this.bank_info_text.TabIndex = 29;
+            // 
+            // bank_info_prompt
+            // 
+            this.bank_info_prompt.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.bank_info_prompt.Location = new System.Drawing.Point(60, 612);
+            this.bank_info_prompt.Name = "bank_info_prompt";
+            this.bank_info_prompt.Size = new System.Drawing.Size(296, 23);
+            this.bank_info_prompt.TabIndex = 30;
+            // 
+            // bank_info_submit
+            // 
+            this.bank_info_submit.Location = new System.Drawing.Point(114, 653);
+            this.bank_info_submit.Name = "bank_info_submit";
+            this.bank_info_submit.Size = new System.Drawing.Size(75, 23);
+            this.bank_info_submit.TabIndex = 31;
+            this.bank_info_submit.Text = "Submit";
+            this.bank_info_submit.Click += new System.EventHandler(this.bank_info_submit_Click);
+            // 
+            // bank_info_no
+            // 
+            this.bank_info_no.Location = new System.Drawing.Point(212, 653);
+            this.bank_info_no.Name = "bank_info_no";
+            this.bank_info_no.Size = new System.Drawing.Size(75, 23);
+            this.bank_info_no.TabIndex = 32;
+            this.bank_info_no.Text = "Nope";
+            this.bank_info_no.Click += new System.EventHandler(this.bank_info_no_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 798);
+            this.Controls.Add(this.bank_info_no);
+            this.Controls.Add(this.bank_info_submit);
+            this.Controls.Add(this.bank_info_prompt);
+            this.Controls.Add(this.bank_info_text);
+            this.Controls.Add(this.bank_info_q);
+            this.Controls.Add(this.credit_card_no);
+            this.Controls.Add(this.phone_number_submit);
+            this.Controls.Add(this.credit_card_prompt);
+            this.Controls.Add(this.credit_card_submit);
+            this.Controls.Add(this.credit_card_q);
+            this.Controls.Add(this.phone_number_q);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.phone_number_prompt);
+            this.Controls.Add(this.phone_number_no);
+            this.Controls.Add(this.credit_card_text);
+            this.Controls.Add(this.phone_number_text);
+            this.Controls.Add(this.title);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.TextBox phone_number_text;
+        private System.Windows.Forms.TextBox credit_card_text;
+        private System.Windows.Forms.Button phone_number_no;
+        private System.Windows.Forms.Label phone_number_prompt;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label phone_number_q;
+        private System.Windows.Forms.Label credit_card_q;
+        private System.Windows.Forms.Button credit_card_submit;
+        private System.Windows.Forms.Label credit_card_prompt;
+        private System.Windows.Forms.Button phone_number_submit;
+        private System.Windows.Forms.Button credit_card_no;
+        private System.Windows.Forms.Label bank_info_q;
+        private System.Windows.Forms.TextBox bank_info_text;
+        private System.Windows.Forms.Label bank_info_prompt;
+        private System.Windows.Forms.Button bank_info_submit;
+        private System.Windows.Forms.Button bank_info_no;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _3280_Assignment1_Program1
+{
+    /// <summary>
+    /// Form class
+    /// </summary>
+        public partial class Form1 : Form
+        {
+        /// <summary>
+        /// Form main method
+        /// </summary>
+            public Form1()
+            {
+                InitializeComponent();
+            //All of the starting text
+            title.Text = "Are you allowed to have a computer?";
+            phone_number_q.Text = "What is your phone number?";
+            phone_number_prompt.Text = "Submitting enters you into a raffle!";
+
+            credit_card_q.Text = "What is your credit card number?";
+            credit_card_prompt.Text = "Work from home for $10,000 a month!";
+
+            bank_info_q.Text = "What is your bank information?";
+            bank_info_prompt.Text = "Wire money to a Nigerian prince to help him access his bank account!";
+        }
+
+
+        /// <summary>
+        /// Clicking no on phone number question 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void phone_number_no_Click(object sender, EventArgs e)
+        {
+            DialogResult MyResult;
+
+            string message = "You chose no. Congratulations, you won't get spam! You can use a computer without doing anything else."; 
+
+            MyResult = MessageBox.Show(message);
+
+            phone_number_prompt.Text = "You chose no."; 
+        }
+
+        /// <summary>
+        /// Clicking submit on phone number question
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void phone_number_submit_Click(object sender, EventArgs e)
+        {
+            DialogResult MyResult;
+
+            string message = "You chose to submit. You entered: " + phone_number_text.Text + ". You will now receive a ton of spam. Please set up call blocking before continuing to use a computer."; 
+
+            MyResult = MessageBox.Show(message); 
+
+            phone_number_prompt.Text = "You submitted: " + phone_number_text.Text;
+        }
+
+
+        /// <summary>
+        /// Clicking no on credit card info question
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void credit_card_no_Click(object sender, EventArgs e)
+        {
+            DialogResult MyResult;
+
+            string message = "You chose no. Congratulations, you won't lose all of your money! You can use a computer without doing anything else."; 
+
+            MyResult = MessageBox.Show(message);
+
+            phone_number_prompt.Text = "You chose no.";
+
+        }
+
+        /// <summary>
+        /// Clicking submit on credit card question 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+            private void credit_card_submit_Click(object sender, EventArgs e)
+        {
+                DialogResult MyResult;
+
+            string message = "You chose to submit. You entered: " + credit_card_text.Text + ". You will now be vulnerable to losing your money. Please contact your bank to cancel your card before continuing to use a computer."; 
+
+            MyResult = MessageBox.Show(message);
+
+                credit_card_prompt.Text = "You submitted: " + credit_card_text.Text;
+            }
+
+        /// <summary>
+        /// Clicking no on bank info question
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bank_info_no_Click(object sender, EventArgs e)
+        {
+            DialogResult MyResult;
+
+            string message = "You chose no. Congratulations, you won't lose $30,000! You can use a computer without doing anything else."; 
+
+            MyResult = MessageBox.Show(message);
+
+            bank_info_prompt.Text = "You chose no.";
+        }
+
+
+        /// <summary>
+        /// Clicking submit on bank info question
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bank_info_submit_Click(object sender, EventArgs e)
+        {
+            DialogResult MyResult;
+
+            string message = "You chose to submit. You entered: " + bank_info_text.Text + ". You will now be vulnerable to losing your money. Please contact your bank to dig you out of this hole before continuing to use a computer."; 
+
+            MyResult = MessageBox.Show(message);
+
+            bank_info_prompt.Text = "You chose no.";
+        }
+    }
+}
